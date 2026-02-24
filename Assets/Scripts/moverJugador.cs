ec8070ba953fdf768ea514f12174c2da3f7b4102
ianjoshua <ianjoshua509@gmail.com>
2026-02-23 20:04:17 -0600

    Agregando modelo escena1

--- Assets/Scripts/moverJugador.cs
+++ Assets/Scripts/moverJugador.cs
@@ -37,27 +37,7 @@ public class moverJugador : MonoBehaviour
     {
         GameObject obj = collision.gameObject;// Obtener el objeto con el que colisionamos (puede ser un punto o un restapunto)
 
-        if (obj.CompareTag("Puntos")) // Si el objeto tiene la etiqueta "Puntos" 
-        {
-            puntos += obj.GetComponent<Puntos>().puntos; // Sumar los puntos del objeto al total de puntos del jugador
-            bocina.PlayOneShot(sonidoPuntos);
-
-            textoPuntos.text = "Puntos: " + puntos;// Actualizar el texto de puntos en la interfaz de usuario
-            Destroy(obj);
-            if (puntos >= 30)// Si los puntos del jugador son menores o iguales a 0, cargar la escena de Game Over (escena con índice 1)
-            {
-                SceneManager.LoadScene(1);
-            }
-        }
-        else if (obj.CompareTag("RestPuntos"))// Si el objeto tiene la etiqueta "RestPuntos"
-        {
-            puntos -= obj.GetComponent<RestPuntos>().puntos;// Restar los puntos del objeto al total de puntos del jugador 
-            bocina.PlayOneShot(sonidoNegativo);
-            textoPuntos.text = "Puntos: " + puntos;// Actualizar el texto de puntos en la interfaz de usuario
-            Destroy(obj);
-
-           
-        }
+       
        
 
     }
